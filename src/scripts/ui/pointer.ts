0e23b111285fa100771595b2af255cc8daa514cf
Masonlet <masonletoile@hotmail.com>
2026-07-17 14:51:15 -0400

    refactor: hoist pointer to engine

--- src/scripts/ui/pointer.ts
+++
@@ -1,8 +0,0 @@
-import {
-  pointerX, pointerY,
-  wasPointerClicked, isPointerDown
-} from "@starweb-libs/engine/input/pointer.js";
-
-export function getPointer() {
-  return { x: pointerX(), y: pointerY(), clicked: wasPointerClicked(), down: isPointerDown() };
-}
